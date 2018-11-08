d3288745d5d5fc71fecd2900e3e734b94fc4c36d
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-11-07 20:57:45 -0500

    Updates

--- precache-manifest.ee0838d8ac97dd941a8300c576251c49.js
+++ precache-manifest.ee0838d8ac97dd941a8300c576251c49.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/react-fullpage/static/js/main.f9255b14.chunk.js"
   },
   {
-    "revision": "7a382838310926d2c004",
-    "url": "/react-fullpage/static/js/1.7a382838.chunk.js"
+    "revision": "464c7fa28254887d1895",
+    "url": "/react-fullpage/static/js/1.464c7fa2.chunk.js"
   },
   {
     "revision": "f9255b1439dedafbf6b2",
     "url": "/react-fullpage/static/css/main.d857b597.chunk.css"
   },
   {
-    "revision": "60df47ac9e1dd6fdafd56c5e0b6d92b9",
+    "revision": "945c6687db5295d3c76da973e6bd0e4f",
     "url": "/react-fullpage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-fullpage/precache-manifest.3d333f4bf7501bc1189ac76e6b287551.js"
+  "/react-fullpage/precache-manifest.ee0838d8ac97dd941a8300c576251c49.js"
 );
 
 workbox.clientsClaim();

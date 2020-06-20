0ffb12d3b61cf79747845665b4038be1b3b3043e
Thierry Charbonnel <t@ap.cx>
2020-06-20 12:09:40 -0400

    Updates

---
+++ precache-manifest.764ef337db19620c1d6e74aff1da7c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470cfe177b1a1caeb626574367b0db7f",
+    "url": "/react-fullpage/index.html"
+  },
+  {
+    "revision": "e3c178edcf2c4dfbb437",
+    "url": "/react-fullpage/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "0aa0491caa638542707c",
+    "url": "/react-fullpage/static/js/2.596854ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fullpage/static/js/2.596854ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c178edcf2c4dfbb437",
+    "url": "/react-fullpage/static/js/main.fc9bbb39.chunk.js"
+  },
+  {
+    "revision": "74a038dac9d2f1e8e68f",
+    "url": "/react-fullpage/static/js/runtime-main.2a0ce687.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-fullpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-fullpage/precache-manifest.a8f6da00dc25cbdda7bc1f48b5d8fb1c.js"
+  "/react-fullpage/precache-manifest.764ef337db19620c1d6e74aff1da7c7d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-fullpage/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-fullpage/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

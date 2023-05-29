16f07c048949dff4e7cfbeb377bd342b403f2063
poop <noahedwardhall@gmail.com>
2023-05-28 23:16:21 -0500

    Chore(deps): add react & react-dom to peer deps

--- src/FPContext.tsx
+++ src/FPContext.tsx
@@ -1,12 +1,12 @@
-import { createContext, type RefObject } from "react";
+import { createContext } from "react";
 
 import { FPItemRef } from ".";
 const observeFn = (el) => el;
 
 export const FPContext = createContext({
   getIndex: (el: any) => 0 as number,
-  ReactFPRef: null as RefObject<HTMLDivElement>,
-  slides: [] as RefObject<HTMLDivElement>[],
+  ReactFPRef: null as FPItemRef,
+  slides: [] as FPItemRef[],
   subscribe: observeFn,
   unsubscribe: observeFn,
 });
